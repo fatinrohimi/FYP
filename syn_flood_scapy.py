a59145fd107d342c96555fa350a35eef4b0fb3ad
fatinrohimi <48275204+fatinrohimi@users.noreply.github.com>
2020-07-12 08:53:30 +0800

    Update syn_flood_scapy.py

--- syn_flood_scapy.py
+++ syn_flood_scapy.py
@@ -6,7 +6,7 @@ p=IP(dst=sys.argv[1],id=1111,ttl=99)/TCP(sport=RandShort(),dport=[22,80],seq=123
 ls(p)
 print ("Sending Packets in 0.3 second intervals for timeout of 4 sec")
 ans,unans=srloop(p,inter=0.3,retry=2,timeout=4)
-print "Summary of answered & unanswered packets"
+print ("Summary of answered & unanswered packets")
 ans.summary()
 unans.summary()
 print ("source port flags in response")
